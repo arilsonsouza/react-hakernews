f5f1838e86265c2667078c6b6211dd3986d5efb3
Arilson Souza <arilsons61@gmail.com>
2018-09-26 19:46:45 -0300

    Interactions with Forms and Events

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ const list = [
   }
 ];
 
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
 
@@ -29,16 +31,32 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list: list,
+      list,
+      searchTerm: ''
     }
   }
+
+  onDismiss = (id) => {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
   
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
-    const helloWorld = 'Welcome to React'
     return (
       <div className="App">
+        <form>
+          <input 
+            type="text"
+            onChange={ this.onSearchChange }  
+          />
+        </form>
         {
-          this.state.list.map((item => 
+          this.state.list.filter(isSearched(this.state.searchTerm)).map((item => 
               <div key={ item.objectID }>
                 <span>
                     <a href={ item.url }>{ item.title }</a>
@@ -46,6 +64,14 @@ class App extends Component {
                 <span>{ item.author }</span>
                 <span>{ item.num_comments }</span>
                 <span>{ item.points }</span>
+                <span>
+                  <button 
+                    onClick={() => this.onDismiss(item.objectID)}
+                    type="button"
+                  >
+                      Dispensar
+                  </button>
+                </span>
               </div>
           ))
         }
