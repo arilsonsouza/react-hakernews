16f859d215052ebead30a2bd4006d6cfd8070bd8
Arilson Souza <arilsons61@gmail.com>
2018-09-27 10:28:03 -0300

    Ref a DOM Element

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './index.css';
 
-const Search = ({ value, onChange,onSubmit ,children }) => {
-    return(
-        <form onSubmit={ onSubmit }>
-            <input 
-                id="search"
-                type="text"
-                value={ value }
-                onChange={ onChange }  
-            />
-            <button type="submit">
-                { children }
-            </button>
-        </form>
-    );
+class Search extends Component {
+
+    componentDidMount(){
+        this.input.focus();
+    }
+    render(){
+        const  { value, onChange,onSubmit ,children } = this.props;
+        return(
+            <form onSubmit={ onSubmit }>
+                <input 
+                    id="search"
+                    type="text"
+                    value={ value }
+                    onChange={ onChange }
+                    ref={ (node) => { this.input = node; } }  
+                />
+                <button type="submit">
+                    { children }
+                </button>
+            </form>
+        );
+    }
 }
 
 export default Search;
